33cc5e7127b4bac06ca4e33cab154b7e12f45487
basesnel <basesnel@gmail.com>
2023-03-11 23:05:07 +0200

    2 task done: style correct

--- src/components/Container.styled.jsx
+++ src/components/Container.styled.jsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 20px 0;
+  padding: 40px 0;
   display: flex;
   align-items: center;
   justify-content: center;
-  overflow: auto;
   background-color: ${props => props.theme.colors.bground};
 `;
