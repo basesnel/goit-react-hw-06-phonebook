376c8922b4ff09b2b98d067f0c8065e46732c142
basesnel <basesnel@gmail.com>
2023-03-12 20:22:45 +0200

    2 task done: Formik try

--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
+import { ErrorMessage, Formik } from 'formik';
+import * as yup from 'yup';
 
-import { ContactFormGetUp } from './ContactForm.styled';
+import { ContactFormGetUp, Input } from './ContactForm.styled';
+
+// const phoneRegExp =
+//   /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/
+
+const scheme = yup.object().shape({
+  name: yup.string().required(),
+  number: yup
+    .string()
+    .matches(
+      /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{2,3}\)?)\s?-?\s?(\(?\d{2,4}\)?)?$/,
+      'Not valid, please try: +0000-000-000-0000'
+    ),
+});
 
 class ContactForm extends React.Component {
   state = { name: '', number: '' };
@@ -16,8 +31,6 @@ class ContactForm extends React.Component {
   };
 
   handleSubmit = event => {
-    event.preventDefault();
-    // console.log(this.state);
     this.props.onSubmit(this.state);
     this.reset();
   };
@@ -28,39 +41,52 @@ class ContactForm extends React.Component {
 
   render() {
     return (
-      <ContactFormGetUp onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId} className="contactform__label">
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            value={this.state.name}
-            onChange={this.handleChange}
-            required
-            id={this.nameInputId}
-            className="contactform__input"
-          />
-        </label>
-        <label htmlFor={this.numberInputId} className="contactform__label">
-          Number
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            value={this.state.number}
-            onChange={this.handleChange}
-            required
-            id={this.numberInputId}
-            className="contactform__input"
-          />
-        </label>
-        <button type="submit" className="contactform__button">
-          Add contact
-        </button>
-      </ContactFormGetUp>
+      <Formik
+        enableReinitialize
+        initialValues={this.state}
+        validationSchema={scheme}
+        onSubmit={this.handleSubmit}
+      >
+        <ContactFormGetUp>
+          <label htmlFor={this.nameInputId} className="contactform__label">
+            Name
+            <Input
+              type="text"
+              name="name"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              value={this.state.name}
+              onChange={this.handleChange}
+              id={this.nameInputId}
+              className="contactform__input"
+            />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className="contactform__error"
+            />
+          </label>
+          <label htmlFor={this.numberInputId} className="contactform__label">
+            Number
+            <Input
+              type="tel"
+              name="number"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              value={this.state.number}
+              onChange={this.handleChange}
+              id={this.numberInputId}
+              className="contactform__input"
+            />
+            <ErrorMessage
+              name="number"
+              component="div"
+              className="contactform__error"
+            />
+          </label>
+          <button type="submit" className="contactform__button">
+            Add contact
+          </button>
+        </ContactFormGetUp>
+      </Formik>
     );
   }
 }
--- src/components/Phonebook/ContactForm/ContactForm.styled.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.styled.jsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
+import { Form, Field } from 'formik';
 
-export const ContactFormGetUp = styled.form`
+export const ContactFormGetUp = styled(Form)`
   margin-bottom: 20px;
 
   .contactform__label {
+    position: relative;
     display: flex;
     gap: 20px;
     justify-content: space-between;
@@ -15,11 +17,16 @@ export const ContactFormGetUp = styled.form`
     border-radius: 4px;
   }
 
-  .contactform__input {
+  .contactform__error {
+    z-index: 100;
+    position: absolute;
+    background-color: yellow;
     padding: 4px;
-    border: 1px solid ${props => props.theme.colors.bground};
-    border-radius: 2px;
-    color: ${props => props.theme.colors.primText};
+    border: 1px solid red;
+    border-radius: 4px;
+    top: 40px;
+    right: 20px;
+    color: red;
   }
 
   .contactform__button {
@@ -38,3 +45,49 @@ export const ContactFormGetUp = styled.form`
     }
   }
 `;
+
+export const Input = styled(Field)`
+  padding: 4px;
+  border: 1px solid ${props => props.theme.colors.bground};
+  border-radius: 2px;
+  color: ${props => props.theme.colors.primText};
+`;
+
+// export const ContactFormGetUp = styled.form`
+//   margin-bottom: 20px;
+
+//   .contactform__label {
+//     display: flex;
+//     gap: 20px;
+//     justify-content: space-between;
+//     align-items: center;
+//     width: 300px;
+//     margin: 0 auto 10px;
+//     padding: 10px 20px;
+//     border: 1px solid ${props => props.theme.colors.bground};
+//     border-radius: 4px;
+//   }
+
+//   .contactform__input {
+//     padding: 4px;
+//     border: 1px solid ${props => props.theme.colors.bground};
+//     border-radius: 2px;
+//     color: ${props => props.theme.colors.primText};
+//   }
+
+//   .contactform__button {
+//     cursor: pointer;
+//     display: block;
+//     width: 160px;
+//     margin: 0 auto;
+//     padding: 4px 8px;
+//     border: 1px solid ${props => props.theme.colors.bground};
+//     color: ${props => props.theme.colors.primText};
+//     background-color: ${props => props.theme.colors.fground};
+//     border-radius: 4px;
+//     font-weight: 700;
+//     &:hover {
+//       background-color: ${props => props.theme.colors.bground};
+//     }
+//   }
+// `;
